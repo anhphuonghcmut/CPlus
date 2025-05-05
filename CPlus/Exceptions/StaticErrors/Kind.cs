e5f26ae0a7a723dcb846fbc9583477d4da2baf90
Anh Phuong <anhphuongdh9a@gmail.com>
2025-05-06 03:42:29 +0700

    Add some exceptions when checking

---
+++ CPlus/Exceptions/StaticErrors/Kind.cs
@@ -0,0 +1,62 @@
+﻿namespace CPlus.Exceptions
+{
+    public class Kind
+    {
+    }
+
+    public class Class : Kind
+    {
+        public override string ToString()
+        {
+            return "Class";
+        }
+    }
+
+    public class Method : Kind
+    {
+        public override string ToString()
+        {
+            return "Class";
+        }
+    }
+
+    public class Attribute : Kind
+    {
+        public override string ToString()
+        {
+            return "Class";
+        }
+    }
+
+    public class Variable : Kind
+    {
+        public override string ToString()
+        {
+            return "Class";
+        }
+    }
+
+    public class Constant : Kind
+    {
+        public override string ToString()
+        {
+            return "Class";
+        }
+    }
+
+    public class Identifier : Kind
+    {
+        public override string ToString()
+        {
+            return "Class";
+        }
+    }
+
+    public class Parameter : Kind
+    {
+        public override string ToString()
+        {
+            return "Class";
+        }
+    }
+}
